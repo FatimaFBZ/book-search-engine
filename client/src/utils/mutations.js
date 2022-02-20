da68d9ef8e27379391d0482e9483c3de864d9c1a
Fatima Baloch <fatima_baluch@hotmail.com>
2022-02-19 19:17:03 -0800

    the first updating

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook( $book: SavedBookInput) {
+    saveBook(book: $book) {
+      _id
+      username
+      bookCount
+      savedBook{
+          bookId 
+          title
+          image
+          description
+          authors
+          link
+
+      }
+      
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      username
+      bookCount
+      savedBook{
+        bookId 
+        title
+        image
+        description
+        authors
+        link
+
+    }
+    }
+  }
+`;
